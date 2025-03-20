c53e0b176edaf6d74428e83c3b3faf54c9cefe21
JassoProg <ernestojassof@gmail.com>
2025-03-20 17:59:32 -0600

    Cambios del GitIgnore

--- DonGalletita_App/Don_galletita/usuarios_app/models.py
+++ DonGalletita_App/Don_galletita/usuarios_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    usuario_id = models.AutoField(primary_key=True)
+    nombre_usuario = models.CharField(max_length=255, unique=True)
+    contrasenia = models.CharField(max_length=255)  
+    rol = models.CharField(
+        max_length=10,
+        choices=[
+            ('admin', 'Admin'),
+            ('empleado', 'Empleado'),
+            ('cliente', 'Cliente')
+        ],
+        default='cliente'
+    )
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    estatus_user = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.nombre_usuario
