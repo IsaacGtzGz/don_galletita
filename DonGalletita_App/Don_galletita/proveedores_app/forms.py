c861a93926fb73bb8d958517b7e66d3693163425
JassoProg <ernestojassof@gmail.com>
2025-04-02 00:21:55 -0600

    Cambios sobre proveedor (Correción de datos)

--- DonGalletita_App/Don_galletita/proveedores_app/forms.py
+++ DonGalletita_App/Don_galletita/proveedores_app/forms.py
@@ -4,12 +4,13 @@ from . import models
 class ProveedorRegistrarForm(forms.ModelForm):
     class Meta:
         model = models.Proveedor
-        fields = ['nombre', 'telefono', 'direccion', 'dia_entrega']
+        fields = ['nombre', 'apellido_paterno', 'apellido_materno', 'telefono', 'direccion']
         widgets = {
             "nombre": forms.TextInput(attrs={"class": "form-control"}),
+            "apellido_paterno": forms.TextInput(attrs={"class": "form-control"}),
+            "apellido_materno": forms.TextInput(attrs={"class": "form-control"}),
             "telefono": forms.TextInput(attrs={"class": "form-control"}),
             "direccion": forms.TextInput(attrs={"class": "form-control"}),
-            "dia_entrega": forms.TextInput(attrs={"class": "form-control"})
         }
 
     def save(self, commit=True):
@@ -22,20 +23,22 @@ class ProveedorRegistrarForm(forms.ModelForm):
 class ProveedorEditarForm(forms.ModelForm):
     class Meta:
         model = models.Proveedor
-        fields = ['nombre', 'telefono', 'direccion', 'dia_entrega']
+        fields = ['nombre', 'apellido_paterno', 'apellido_materno', 'telefono', 'direccion']
         widgets = {
             "nombre": forms.TextInput(attrs={"class": "form-control"}),
+            "apellido_paterno": forms.TextInput(attrs={"class": "form-control"}),
+            "apellido_materno": forms.TextInput(attrs={"class": "form-control"}),
             "telefono": forms.TextInput(attrs={"class": "form-control"}),
             "direccion": forms.TextInput(attrs={"class": "form-control"}),
-            "dia_entrega": forms.TextInput(attrs={"class": "form-control"})
         }
 
     def save(self, id, commit=True):
         proveedor = models.Proveedor.objects.filter(proveedor_id=id).first()
         proveedor.nombre = self.cleaned_data["nombre"]
+        proveedor.apellido_paterno = self.cleaned_data["apellido_paterno"]
+        proveedor.apellido_materno = self.cleaned_data["apellido_materno"]
         proveedor.telefono = self.cleaned_data["telefono"]
         proveedor.direccion = self.cleaned_data["direccion"]
-        proveedor.dia_entrega = self.cleaned_data["dia_entrega"]
         if commit:
             proveedor.save()
         return proveedor
--- DonGalletita_App/Don_galletita/proveedores_app/views.py
+++ DonGalletita_App/Don_galletita/proveedores_app/views.py
@@ -1,42 +1,44 @@
-from django.shortcuts import get_object_or_404, redirect
-from proveedores_app.models import Proveedor
+from django.shortcuts import get_object_or_404
 from django.views.generic.base import TemplateView
 from django.views.generic import FormView, DeleteView
-from . import forms
 from django.urls import reverse_lazy
+from proveedores_app.models import Proveedor
+from . import forms
 
-# Create your views here.
+# Lista de proveedores
 class ListaProveedoresView(TemplateView):
     template_name = 'lista_proveedores.html'
 
-    def get_context_data(self):
-        lista = Proveedor.objects.all()
-        return {'lista': lista}
-    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['lista'] = Proveedor.objects.all()
+        return context
+
 # Crear un proveedor
 class CrearProveedorView(FormView):
-    template_name = 'crear_proveedor.html'  
+    template_name = 'crear_proveedor.html'
     form_class = forms.ProveedorRegistrarForm
     success_url = reverse_lazy('lista_proveedores')
+
     def form_valid(self, form):
-        form.save(self.request)
+        form.save()  # Se asume que el formulario guarda directamente el modelo
         return super().form_valid(form)
-    
-# Editar un proveedor    
+
+# Editar un proveedor
 class EditarProveedorView(FormView):
-    template_name = 'editar_proveedor.html'  
+    template_name = 'editar_proveedor.html'
     form_class = forms.ProveedorEditarForm
     success_url = reverse_lazy('lista_proveedores')
+
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         id = self.kwargs.get('id')
         proveedor = get_object_or_404(Proveedor, proveedor_id=id)
-        kwargs['instance'] = proveedor
+        kwargs['instance'] = proveedor  # Pasar la instancia al formulario
         return kwargs
-    
+
     def form_valid(self, form):
-        id = self.kwargs.get('id')
-        form.save(id=id)
+        form.save()  # Se asume que el formulario guarda directamente el modelo
         return super().form_valid(form)
 
 # Eliminar un proveedor
