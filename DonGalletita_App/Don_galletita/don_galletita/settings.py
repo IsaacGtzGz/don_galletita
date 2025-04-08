5823d6d1233314b18366529b43e595d06fee087c
JassoProg <ernestojassof@gmail.com>
2025-04-08 13:07:16 -0600

    Cambios de diseño y funcionalidad

--- DonGalletita_App/Don_galletita/don_galletita/settings.py
+++ DonGalletita_App/Don_galletita/don_galletita/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'don_galletita_bd',
         'USER': 'root',
-        'PASSWORD': '251221',
+        'PASSWORD': 'cclab',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
